68b3099668e9ffafaee94212d1d9c168420fb65a
Arinze <60965900+arinze090@users.noreply.github.com>
2022-01-30 14:17:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Footer from './components/Footer/footer';
+import Navbar from './components/Navbar/Navbar';
+import Banner from './components/Banner';
+import About from './components/About';
+import Projects from './components/Projects';
+import ScrollButton from './components/ScrollToTop/ScrollButton';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Banner />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+      <ScrollButton />
+    </Router>
   );
 }
 
